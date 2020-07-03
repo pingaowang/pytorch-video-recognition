e030b582f388bb9113f37047d4fae9303bd05e8b
pingaowang <pingaowang2015@gmail.com>
2020-07-03 14:42:52 -0400

    new exp group

---
+++ exp_group/group0008_seizure17.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/home/pingao/anaconda3/envs/tf/bin/python train.py exp0008_seizurev17_01.yaml
+#/home/pingao/anaconda3/envs/tf/bin/python train.py exp0008_seizurev17_02.yaml
+/home/pingao/anaconda3/envs/tf/bin/python train.py exp0008_seizurev17_03.yaml
---
+++ exp_group/group0008_seizure17_adam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/jizong/.conda/envs/pingao/bin/python train.py exp0008_seizurev17_adam_01.yaml
+/home/jizong/.conda/envs/pingao/bin/python train.py exp0008_seizurev17_adam_02.yaml
+/home/jizong/.conda/envs/pingao/bin/python train.py exp0008_seizurev17_adam_03.yaml
