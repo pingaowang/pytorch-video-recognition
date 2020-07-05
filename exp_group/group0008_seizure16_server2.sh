925cf67ab6ece667170d956b7988fca4acae18be
pingaowang <pingaowang2015@gmail.com>
2020-07-04 22:40:18 -0400

    new exp group: seizure_v16

---
+++ exp_group/group0008_seizure16_server2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/jizong/.conda/envs/pingao/bin/python train.py exp0008_seizurev16_11_server2.yaml
+/home/jizong/.conda/envs/pingao/bin/python train.py exp0008_seizurev16_12_server2.yaml
+/home/jizong/.conda/envs/pingao/bin/python train.py exp0008_seizurev16_13_server2.yaml
